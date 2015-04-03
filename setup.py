7b4d5093c58bcbff1e3f2083223c3777c2db217f
homeway <xiaocao.grasses@gmail.com>
2015-04-03 09:47:53 +0800

    Beta Version 15.04.03

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'ultrasound',
-    version = '15.04.02',
+    version = '15.04.03',
     keywords = ('raspberry pi', 'ultrasound'),
     description = 'ultrasound module for raspberry pi',
     license = 'MIT License',
